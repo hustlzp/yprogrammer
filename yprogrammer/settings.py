e77ec2609cc43588b612b7c2c8a59a956d113da2
hustlzp <hustlzp@qq.com>
2013-08-06 23:09:30 +0800

    set font size to 12.5px to fix the displaying bug of icon-bookmark-empty in windows

--- yprogrammer/settings.py
+++ yprogrammer/settings.py
@@ -89,7 +89,6 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    # 'django.middleware.csrf.CsrfResponseMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
