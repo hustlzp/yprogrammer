4f91df381831a98093efcc4e0ddc739871344a67
hustlzp <hustlzp@qq.com>
2013-08-19 08:39:03 +0800

    add local setting sample file

---
+++ yprogrammer/local_settings_sample.py
@@ -0,0 +1,20 @@
+DEBUG = True
+
+ALLOWED_HOSTS = ['http://www.yprogrammer.com']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'yprogrammer',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    }
+}
+
+# GitHub OAuth params
+CLIENT_ID = ''
+CLIENT_SECRET = ''
+REDIRECT_URL = ''
