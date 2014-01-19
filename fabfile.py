513f17b137d822f0addef0b512516e250e004bb8
hustlzp <hustlzp@qq.com>
2014-01-19 19:17:41 +0800

    update fab file

--- fabfile.py
+++ fabfile.py
@@ -1,21 +1,29 @@
 from fabric.api import run, env, cd
 
-env.host_string = "root@184.22.89.208"
+host_string = "root@192.241.159.34"
 
-def start():
-    with cd('/var/www/yprogrammer'):
-        run('git pull')
-        run('. venv/bin/active')
-        run('gunicorn -c gunicorn.conf yprogrammer.wsgi:application -D')
+
+def first():
+    env.host_string = host_string
+    with cd('/var/www/lishengchun'):
+        # nginx
+        run('cp nginx.conf /etc/nginx/sites-available/yp')
+        run('ln -sf /etc/nginx/sites-available/yp /etc/nginx/sites-enabled/')
+        run('nginx -s reload')
+
+        # supervisor
+        run('cp supervisor.conf /etc/supervisor/conf.d/yp.conf')
+        run('supervisorctl reread')
+        run('supervisorctl update')
+
 
 def restart():
-    run('kill -HUP `cat /tmp/yprogrammer.pid`')
+    env.host_string = host_string
+    run('supervisorctl restart yp')
+
 
 def deploy():
+    env.host_string = host_string
     with cd('/var/www/yprogrammer'):
         run('git pull')
-        run('kill -HUP `cat /tmp/yprogrammer.pid`')
-
-def ldeploy():
-    with cd('/var/www/yprogrammer'):
-        run('git pull')
+        run('supervisorctl restart yp')
